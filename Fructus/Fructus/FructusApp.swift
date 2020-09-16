89dab44072372ab8595550f2a615d77adc63bd84
Михаил <40540053+egorovmikhail@users.noreply.github.com>
2020-09-16 23:07:40 +1100

    Add files via upload

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Михаил Егоров on 16.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
